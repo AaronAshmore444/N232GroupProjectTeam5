1903d48d3946e5ec3edca443d2a7c16153e33160
AaronAshmore444 <aaashmor@iu.edu>
2026-04-11 01:23:07 -0400

    Fixed Player Movement - AAA

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     //Player Movement Speed
     public float moveSpeed = 5f;
+    public float sprintSpeed = 10f;
     //Sets Player Jump force Variable
+
+    public float verticalVelocity = 0f;
     public float jumpStrength = 5f;
-    //Sets Player Rigid Body variable
-    private Rigidbody rb;
+    
+    public CharacterController controller;
+
+    public Transform cameraTransform;
+
     //Bool to Check if on the ground
     public bool OnGround = true;
     //Bool to turn on player jump
     public bool doJump = false;
-    //Creates variable to choose smoothing
-    [SerializeField] private float smoothTime;
     //Spawnpoint for thrown Gadget
     public Transform throwPoint;
     //Force for thrown gadget
@@ -26,17 +30,53 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Finds Player Rigid Body
-        rb = GetComponent<Rigidbody>();
+        
+        
     }
     // Update is called once per frame
     void Update()
     {
-        //Tells player if space is pressed then it is allowed to jump (For some reason this was inconsistent in Void FixedUpdate)
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        OnGround = controller.isGrounded;
+
+        if (OnGround && verticalVelocity < 0)
         {
-            doJump = true;
+            verticalVelocity = -2f;
         }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        movement = cameraTransform.TransformDirection(movement);
+        movement.y = 0;
+        movement = movement.normalized;
+        
+        float currentSpeed = moveSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift) && OnGround)
+        {
+            currentSpeed = sprintSpeed;
+        }
+
+        
+
+        
+
+        
+        //Tells player if space is pressed then it is allowed to jump (For some reason this was inconsistent in Void FixedUpdate)
+        if (Input.GetKeyDown(KeyCode.Space) && OnGround)
+        {
+            verticalVelocity = jumpStrength;
+        }
+
+        if (!OnGround)
+        {
+            verticalVelocity += Physics.gravity.y * Time.deltaTime;
+        }
+        Vector3 allMovement = movement * currentSpeed;
+        allMovement.y = verticalVelocity;
+        controller.Move(allMovement * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.R))
         {
@@ -47,36 +87,8 @@ public class PlayerController : MonoBehaviour
 
 
     void FixedUpdate()
-    {
-        //Sets player horizontal and vertical variables
-
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        //Sets player Velocity and tell player to move in chosen axis direction
-        Vector3 move = new Vector3(h * moveSpeed, rb.linearVelocity.y, v * moveSpeed);
-
-        rb.linearVelocity = move;
-
-        //Checks if Space button was pressed and if player is on the ground plane, then applys upward force if so
-        if (doJump && OnGround)
-        {
-            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
-            doJump = false;
-        }
-        // Sets Player Rotation to pressed axis key
-        Vector3 spin = new Vector3(h, 0, v);
-
-        if (spin != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(spin * smoothTime);
-        }
-
-        // If Left Shift is pressed, player sprints at double speed
-        if (Input.GetKey(KeyCode.LeftShift) && OnGround)
-        {
-            rb.linearVelocity *= 2f;
-        }
+    { 
+      
         // If F is pressed, player throws the Trap
         if (Input.GetKeyDown(KeyCode.F) && gadgetToThrow != null)
         {
@@ -104,11 +116,6 @@ public class PlayerController : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
 
-        //If player is touching the ground plane, then enable jump
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            OnGround = true;
-        }
 
 
         //If enemy touches player, player dies 
@@ -125,12 +132,5 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void OnCollisionExit(Collision collision)
-    {
-        //If player is not touching the ground plane, then disable jump
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            OnGround = false;
-        }
-    }
+    
 }
