716297c51b20096465db2cb4518946e09a555421
StaxxDiff <darondiff@gmail.com>
2026-04-14 19:05:54 -0400

    Updated Spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,8 +7,14 @@ public float spawnInterval = 2f;
 private float timer = 5f;
 private int enemiesSpawned = 0;
 public int maxEnemies = 5;
+public float spawnRadius = 5f;
 void Update()
 {
+Vector3 offset = new Vector3(
+    Random.Range(-spawnRadius, spawnRadius),
+    0,
+    Random.Range(-spawnRadius, spawnRadius)
+);
 if (enemiesSpawned >= maxEnemies)
     return;
 timer += Time.deltaTime;
@@ -16,11 +22,12 @@ if (timer >= spawnInterval)
 {
 GameObject newObject= Instantiate(
 spawnPrefab,
-transform.position,
+transform.position + offset,
 Quaternion.identity
 );
 timer = 0f;
 enemiesSpawned++;
 }
 }
+
 }
